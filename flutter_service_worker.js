69e674af9400dc1a59db417cbd30a9bb8bc293c1
Carlos Castro <55493437+CarlosCastroWolf@users.noreply.github.com>
2020-05-20 01:39:59 -0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "6cf4ba39c467f202234f4c8d8ea38c47",
 "/": "6cf4ba39c467f202234f4c8d8ea38c47",
-"main.dart.js": "06c77da79fc251c9b53e5b6fce1cf28e",
+"main.dart.js": "74d6c7c8700c1fa49088bf29a416accd",
 "manifest.json": "be6d640eb37af5bf2dcf15893e2084b2"
 };
 
